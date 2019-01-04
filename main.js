497ec99f78ebed99afacf052e481d75d1984b643
Owen D'Aprile <17219838+owendaprile@users.noreply.github.com>
2019-01-03 21:54:02 -0500

    lots of new cool things

--- main.js
+++ main.js
@@ -1,7 +1,22 @@
-const {app, BrowserWindow} = require('electron')
+const {app, BrowserWindow, Menu, Tray} = require('electron')
 const windowStateKeeper = require('electron-window-state')
+var path = require('path')
 
 let window
+let isQuitting
+let isPlaying
+let tray = null
+
+const trayMenu = [
+    {label: 'Play/Pause', click: () => {
+        window.webContents.executeJavaScript('document.getElementById("play-pause-button").click()')
+    }},
+    {label: 'Exit', click: () => {
+        isQuitting = true
+        tray.destroy()
+        app.quit()
+    }},
+]
 
 function createWindow() {
     let mainWindowState = windowStateKeeper({
@@ -13,7 +28,21 @@ function createWindow() {
         'x': mainWindowState.x,
         'y': mainWindowState.y,
         'width': mainWindowState.width,
-        'height': mainWindowState.height
+        'height': mainWindowState.height,
+        'icon': path.join(__dirname, 'assets/icons/win/96x96.ico'),
+        'title': 'YouTube Music'
+    })
+
+    tray = new Tray(path.join(__dirname, 'assets/icons/win/32x32.ico'))
+    const contextMenu = Menu.buildFromTemplate(trayMenu)
+
+    tray.setToolTip('YouTube Music')
+    tray.setContextMenu(contextMenu)
+
+    tray.on('click', () => {
+        if(!window.isVisible()) {
+            window.show()
+        }
     })
 
     mainWindowState.manage(window)
@@ -22,8 +51,17 @@ function createWindow() {
 
     window.loadURL('https://music.youtube.com')
 
+    window.webContents.openDevTools()
+
     window.on('closed', () => {
         window = null
+    })
+
+    window.on('close', function(event) {
+        if(!isQuitting) {
+            event.preventDefault()
+            window.hide()
+        }
     })
 }
 
